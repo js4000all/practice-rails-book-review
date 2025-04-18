e1387a3f5f71b211b370bfc7924c42d3adb8a170
js4000all <js4000all@gmail.com>
2025-04-18 11:57:18 +0000

    feat: 書籍管理機能を追加し、コントローラー、ビュー、ヘルパー、ルーティング、テストを実装

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: '書籍が追加されました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :publisher, :published_year)
+  end
+end
